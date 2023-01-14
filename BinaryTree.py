238cd2c79eb2a2629470edc08c4ce9f849af21cd
SenyoGela <senogelamariconb@gmail.com>
2023-01-15 07:23:40 +0800

    add post order traverse method

--- BinaryTree.py
+++ BinaryTree.py
@@ -40,3 +40,12 @@ class BinarySearchTreeNode:
         if self.right:
             elements += self.right.in_order_traversal()
         return elements
+
+    def post_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.post_order_traversal()
+        if self.right:
+            elements += self.right.post_order_traversal()
+        elements.append(self.data)
+        return elements
